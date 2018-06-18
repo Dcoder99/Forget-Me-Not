86997d296453f1ba7cd11d3701d0c3266205d29b
Anagha <anagha.ananth7@gmail.com>
2018-06-18 15:34:46 +0530

    Made changes to Tile Game
    (high score feature to be added)

---
+++ GameDev/2D App/Assets/DisplayFinalScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+
+public class DisplayFinalScore : MonoBehaviour {
+	public Text FinalScore;
+	int FinalS;
+
+	public void GoBackToGenerate()
+	{
+		Application.LoadLevel("For Generating");
+
+	}
+	// Use this for initialization
+	void Start () {
+		FinalScore.text= "Final Score: " + ToGenerate.score.ToString();
+		ToGenerate.score=0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
