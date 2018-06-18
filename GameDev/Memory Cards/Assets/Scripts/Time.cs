7d672b1767bab98b6e0a8922c69987d5212511d1
Dhruv Vohra <dhruv.v405@gmail.com>
2018-06-18 13:47:56 +0530

    Added Memory Cards game

---
+++ GameDev/Memory Cards/Assets/Scripts/Time.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Time : MonoBehaviour {
+
+    float Timeleft = 30f;
+    public Text time_display;
+	
+	void Update () {
+        Timeleft -= UnityEngine.Time.deltaTime;		
+	}
+
+    private void OnGUI()
+    {
+        if (Timeleft > 0f)
+        {
+            time_display.text = "Time: " + Timeleft.ToString("0");
+        }
+
+        else if (Timeleft < 0)
+        {
+            SceneManager.LoadScene("End Game");
+        }
+        
+    }
+}
