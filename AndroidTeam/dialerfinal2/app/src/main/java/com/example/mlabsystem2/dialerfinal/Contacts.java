86b39d04d7c418309902bd6ec27eafd55273e336
mlabSystem2 <deeps.karanji2@gmail.com>
2018-06-21 03:30:07 -1000

    dialer finished

---
+++ AndroidTeam/dialerfinal2/app/src/main/java/com/example/mlabsystem2/dialerfinal/Contacts.java
@@ -0,0 +1,51 @@
+package com.example.mlabsystem2.dialerfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Debug;
+import android.provider.CallLog;
+import android.provider.ContactsContract;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class Contacts extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    ArrayList<String> contacts=new ArrayList<String>();
+    ArrayList<String> numbers=new ArrayList<String>();
+    Cursor cursor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+        startManagingCursor(cursor);
+        while (cursor.moveToNext()){
+            contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
+            numbers.add(cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+        }
+        recyclerView=findViewById(R.id.recyclerView);
+        layoutManager=new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter=new RecyclerAdapter(contacts,numbers,cursor,this);
+        recyclerView.setAdapter(adapter);
+    }
+}
