608f7c44a57a9cea2453ad00ac42d4fac91efd5d
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-06-25 16:12:46 +0530

    Added checkpoint

--- GameDev/Pyramid Run/Assets/Scripts/Collisions.cs
+++ GameDev/Pyramid Run/Assets/Scripts/Collisions.cs
@@ -1,24 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Collisions : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+public class Collisions : MonoBehaviour
+{
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Wall L" || collision.gameObject.name == "Wall R")
+        string[] strArr = null;
+        char[] splitchar = { ' ' };
+        strArr = (collision.gameObject.name).Split(splitchar);
+
+        // Debug.Log("Collided with " + collision.gameObject.name);
+        if (strArr[0] == "coin")
         {
-           // Movements.runState = false;
+            if (strArr[1] == GameScript.targetIdx.ToString())
+            {
+                Destroy(collision.gameObject);
+                Debug.Log("CORRECT ORDER");
+                GameScript.targetIdx++;
+
+                if (GameScript.targetIdx > GameScript.maxCoinIdx)
+                {
+                    Debug.Log("LEVEL COMPLETED!");
+                    // Game finished
+                    // Change scene
+                }
+            }
+            else
+            {
+                // message.text = "WRONG CHECKPOINT, GO TO " + strArr[1];
+                Debug.Log("WRONG ORDER.");
+                //Reduce stars
+            }
         }
     }
 }
