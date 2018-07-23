f3656bf72b53ce93396272ffa4fca65852caf133
Aniket Kaulavkar <aniket.kaulavkar@gmail.com>
2018-07-23 10:19:51 +0530

    Sign In activity fixed

--- AndroidTeam/Caretaker/app/src/main/java/com/example/mlabsystem2/setup_2/PatientMenu.java
+++ AndroidTeam/Caretaker/app/src/main/java/com/example/mlabsystem2/setup_2/PatientMenu.java
@@ -19,62 +19,16 @@ import java.util.List;
 
 public class PatientMenu extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 123;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patientmenu);
-
-        //        Do this in the main activity for Sign-in
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
-
-        // Create and launch sign-in intent
-        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-        String uid = prefs.getString("uid", "");
-        if (uid.equals("")) {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(),
-                    RC_SIGN_IN);
-        }
-        //      ..........................
-
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                String uid = user.getUid();
-                SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("uid", uid);
-                editor.apply();
-
-                // ...
-            } else {
-                Toast.makeText(this, "Couldn't sign you in. Please check your internet connectivity.", Toast.LENGTH_SHORT).show();
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-            }
-        }
-
-
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
     }
 
     public void SelectProfile(View view) {
