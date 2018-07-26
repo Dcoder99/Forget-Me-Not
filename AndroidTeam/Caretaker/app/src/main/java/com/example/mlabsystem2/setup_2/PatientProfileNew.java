23a37adbe8819dfee8b5d7f5fbcf0886b8975141
Vishnu S Murali <muralisvishnu@gmail.com>
2018-07-26 23:35:54 +0530

    caretaker with gps

--- AndroidTeam/Caretaker/app/src/main/java/com/example/mlabsystem2/setup_2/PatientProfileNew.java
+++ AndroidTeam/Caretaker/app/src/main/java/com/example/mlabsystem2/setup_2/PatientProfileNew.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentReference;
@@ -22,12 +24,14 @@ import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class PatientProfileNew extends AppCompatActivity {
+public class PatientProfileNew extends AppCompatActivity implements View.OnClickListener {
 
     String TAG = "MEEEE";
+    Button  trackLocation;
     FirebaseFirestore db;
     String patient_uid;
     TextView patientName, patientNumber;
+    ArrayList<String> coordinates=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +54,40 @@ public class PatientProfileNew extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         patient_uid = prefs.getString("patient_uid", "");
 
+        final DocumentReference docRef = db.collection("Patients").document(patient_uid);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+
+
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+                    coordinates = new ArrayList<>();
+                    Map<String, Object> details = (Map<String, Object>) snapshot.get("coordinates");
+                    Log.d("MEEEE", "onComplete: " + details);
+                    if (details != null) {
+                        coordinates.add(details.get("latitude").toString());
+                        coordinates.add(details.get("longitude").toString());
+
+
+                    }
+
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
+
+        trackLocation=findViewById(R.id.button3);
         patientName = findViewById(R.id.patient_name);
         patientNumber = findViewById(R.id.patient_number);
+        trackLocation.setOnClickListener(this);
 
         loadDetails();
     }
@@ -111,10 +147,14 @@ public class PatientProfileNew extends AppCompatActivity {
         finish();
     }
 
-    public void launchTrackLocation(View view) {
-        Intent intent = new Intent(this, LocationActivity.class);
-        startActivity(intent);
-    }
+    public void LaunchMap(View view,ArrayList<String>coordinates) {
+
+            Uri gmmIntentUri = Uri.parse("google.navigation:q="+Double.parseDouble(coordinates.get(0))+","+Double.parseDouble(coordinates.get(1)));
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+            }
+
 
     public void launchScheduleActivity(View view) {
         Intent intent = new Intent(this, ScheduleMain.class);
@@ -139,5 +179,10 @@ public class PatientProfileNew extends AppCompatActivity {
     public void editDetails(View view) {
         Intent intent = new Intent(this, EditPatientDetails.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        LaunchMap(v,coordinates);
     }
 }
